0476838e4ef8596511b1efa1f184c42a42d44fce
andreleblanc11 <andreleblanc725@gmail.com>
2023-12-16 15:02:43 -0400

    Add smarter logic for multi monitor setup

--- bin/DualMonitorSetUp.sh
+++ bin/DualMonitorSetUp.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 # Set up two monitors with bspwm
+# The order that the monitor appears on xrandr determines its placement from the left side of the laptop monitor.
 
-external_monitor = $(xrandr --query | grep 'HDMI-2')
+external_monitors=$(xrandr --query | grep 'DVI' | cut -d ' ' -f1)
+count=0
 
-if [[ $external_monitor = *connected* ]];then
-	xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal --output HDMI-2 --mode 1920x1080 --rotate normal --right-of eDP-1
-fi
+for monitor in $external_monitors;
+do
+	echo $monitor
+	count=$((count+1))
+	if [[ $count == 1 ]]; then
+ 		xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal --output $monitor --mode 1920x1080 --rotate normal --left-of eDP-1
+		monitor1=$monitor
+	else
+ 		xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal --output $monitor --mode 2560x1440 --rotate normal --left-of $monitor1
+	fi
 
+done
