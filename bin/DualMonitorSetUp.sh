5992581554af407ec02f5be48899374f3d4f1f4f
andreleblanc11 <andreleblanc725@gmail.com>
2023-12-17 15:57:24 -0400

    Recorrect Dual Monitor startup script

--- bin/DualMonitorSetUp.sh
+++ bin/DualMonitorSetUp.sh
@@ -2,19 +2,14 @@
 
 # Set up two monitors with bspwm
 # The order that the monitor appears on xrandr determines its placement from the left side of the laptop monitor.
+#
 
 external_monitors=$(xrandr --query | grep 'DVI' | cut -d ' ' -f1)
-count=0
 
-for monitor in $external_monitors;
-do
-	echo $monitor
-	count=$((count+1))
-	if [[ $count == 1 ]]; then
- 		xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal --output $monitor --mode 1920x1080 --rotate normal --left-of eDP-1
-		monitor1=$monitor
-	else
- 		xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal --output $monitor --mode 2560x1440 --rotate normal --left-of $monitor1
-	fi
+if [[ $external_monitors == *"DVI-I-1-1"* ]]; then
+xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal --output "DVI-I-1-1" --mode 1920x1080 --rotate normal --left-of eDP-1
+fi
 
-done
+if [[ $external_monitors == *"DVI-I-2-2"* ]]; then
+xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal --output "DVI-I-2-2" --mode 2560x1440 --rotate normal --left-of "DVI-I-1-1"
+fi
