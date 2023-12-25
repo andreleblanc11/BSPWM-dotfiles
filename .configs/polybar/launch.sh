b6cce0917a1163581197bb4b8d787f43b757cf56
andreleblanc11 <andreleblanc725@gmail.com>
2023-12-25 10:27:37 -0400

    Add pull_latest_changes script and pull latest changes

--- .configs/polybar/launch.sh
+++ .configs/polybar/launch.sh
@@ -7,17 +7,17 @@ killall -q polybar
 polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar monitor1 -c ~/.config/polybar/config.ini | tee -a /tmp/polybar.log & disown
+polybar monitor1 -c ~/.config/polybar/config.ini | tee -a ~/.local/log/polybar/polybar.log & disown
 
 # We also want to launch polybar on the other displays (on the docking station)
 external_monitor1 = $(xrandr --query | grep 'DVI-I-1-1')
 if [[ $external_monitor1 = *connected* ]]; then
-	polybar monitor2 -c ~/.config/polybar/config.ini | tee -a /tmp/polybar.log & disown
+	polybar monitor2 -c ~/.config/polybar/config.ini | tee -a ~/.local/log/polybar/polybar.log & disown
 fi
 
 external_monitor2 = $(xrandr --query | grep 'DVI-I-2-2')
 if [[ $external_monitor2 = *connected* ]]; then
-	polybar monitor3 -c ~/.config/polybar/config.ini | tee -a /tmp/polybar.log & disown
+	polybar monitor3 -c ~/.config/polybar/config.ini | tee -a ~/.local/log/polybar/polybar.log & disown
 fi
 
 
