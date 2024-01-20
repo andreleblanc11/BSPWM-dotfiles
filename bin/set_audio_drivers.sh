f11aa7d95b2b175dfa0aaebbf81fdcd673febdae
andreleblanc11 <andreleblanc725@gmail.com>
2024-01-20 11:44:04 -0400

    Correct audio driver startup script

--- bin/set_audio_drivers.sh
+++ bin/set_audio_drivers.sh
@@ -3,8 +3,8 @@
 # 
 # 
 
-check_drivers=$(pacmd list-sources | grep alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source)
+check_drivers=$(pacmd list-sources | grep alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink)
 
 if [[ $check_drivers ]]; then
-	pacmd set-default-source alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__source
+	pacmd set-default-sink alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink
 fi
